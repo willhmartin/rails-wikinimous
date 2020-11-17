37185f4a50272f3063d90fa2135146f1502d0a0d
Will Martin <whmartinwork@outlook.com>
2020-11-17 17:02:33 +0800

    finished 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/create'
+  get 'articles/update'
+  get 'articles/destroy'
+  get 'articles/edit'
+  get 'articles/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
